e478eda536b36791a0e5a0541fddcfc3b654e14c
erfanRmoghadam <Erfanramezani47@gmail.com>
2025-02-02 02:15:54 -0800

    Add files via upload

---
+++ project.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+#include <iomanip>
+#include <vector>
+using namespace std;
+
+struct student{
+    string name;
+    int id;
+    string reshteh;
+    double moaadel;
+
+
+};
+
+int main(){
+    student s;
+    cin >> s.name >> s.id >> s.reshteh >> s.moaadel;
+    cout << s.name << "  " << s.id << "  " << s.reshteh << "  " << s.moaadel << endl;
+
+
+
+
+
+
+
+
+
+
+}
